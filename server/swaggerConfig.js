9865d36eeac5844ef50c27e957729190b07c4dc8
solaimanb <solaimanb.dev@gmail.com>
2025-05-13 12:14:20 +0600

    update server & workspace config

--- server/swaggerConfig.js
+++ server/swaggerConfig.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Kustia Arts School",
+      title: "Kustia Fine Arts & Technology School API",
       version: "1.0.0",
       description: "API documentation with Swagger",
     },
